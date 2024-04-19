a8de0e38ff7069154359492a4cf0c2b728a42cc0
Kaifarikzxc <147406196+Kaifarikzxc@users.noreply.github.com>
2024-04-19 10:39:19 +0300

    Add files via upload

---
+++ здд1/здд1/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace здд1
+{
+    class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+
+        public int CompareTo(Book book1)
+        {
+            return string.Compare(this.Title, book1.Title);
+        }
+    }
+}
+
---
+++ здд1/здд1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace здд1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>
+        {
+            new Book { Title = "a", Price = 15 },
+            new Book { Title = "Гарри Потер", Price = 12 },
+            new Book { Title = "Мастер и Маргарита", Price = 10 },
+            new Book { Title = "Вии", Price = 14 },
+            new Book { Title = "Преступление и наказание", Price = 11 }
+        };
+
+            books.Sort();
+
+            foreach (var book in books)
+            {
+                Console.WriteLine($"Название: {book.Title}, Цена:{book.Price}");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
+  
